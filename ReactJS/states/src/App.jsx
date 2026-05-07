6dbbc40b963022d050f6007b3b6bb0f7487bbefa
THIAGO ESPERIDIÃO MOURA RIBEIRO <49631963870@info.local>
2026-05-07 16:50:07 -0300

    Realizado os inputs do state

---
+++ ReactJS/states/src/App.jsx
@@ -0,0 +1,27 @@
+ import {useState} from "react"
+
+function App () {
+
+  const[nome, setNome] = useState("Google")
+
+  function trocarTexto () {
+    setNome("Microsoft")
+  }
+
+  function fuiAbandonado(){
+    setNome ("Filho da puta escreve ai >:(")
+  }
+
+  return(
+    <>
+    <h1>{nome} Page</h1>
+    <button onClick={trocarTexto}>Mudar Texto</button>
+    <button onClick= {()  => {return setNome ("Yahoo")}}>Mudar Texto</button>
+    <br />
+    <input type="text" onBlur={fuiAbandonado} onChange={(evento) => setNome(evento.target.value)}/>
+    <Contador/>
+    </>
+  ) 
+}
+
+export default App;
